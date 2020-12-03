5c2fd400330b0cb00162a632b5010d28a9521f83
gabri <gabri@DESKTOP-7DH2TF6>
2020-12-03 00:39:49 -0200

    Init

---
+++ src/testegraficos/TesteGraficos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testegraficos;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.MinMaxCategoryRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.*;
+import org.jfree.ui.RefineryUtilities;
+
+
+/**
+ *
+ * @author gabri
+ */
+public class TesteGraficos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
